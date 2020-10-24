f61ccdb9d6854f1f0c6a6b5b8a5e7d52f77ecbd4
omkarmane2489 <58086150+omkarmane2489@users.noreply.github.com>
2020-10-24 21:10:47 +0530

    Update run.py
    Updated

--- MOS_Project/data/run.py
+++ MOS_Project/data/run.py
@@ -1,11 +1,15 @@
 from data.data_insert import *
 
 if __name__ == '__main__':
-
+    
+    # Function for creating elements table 
     create_element_table()
-
+    
+    # Function for inserting elements data into DB
     insert_element_data()
 
+    # Function for creating commodity table
     create_commodity_table()
 
+    # Function for inserting commodity data
     insert_commodity_data()
